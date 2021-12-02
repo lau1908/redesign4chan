a6450fc4e4d289e3098ca080f78de8eff283dd67
Lau Løgager Sørensen <laux0488@college360.dk>
2021-12-02 18:37:57 +0100

    Add project files.

---
+++ redesign4chan/Controllers/BoardController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace redesign4chan.Controllers
+{
+    public class BoardController : Controller
+    {
+        ny4chanEntities databaseManager = new ny4chanEntities();//jeg erklærer en entiti af min database til objektet "databaseManager"
+        // GET: Board
+        public ActionResult Show(int id)//
+        {
+            //Debug.WriteLine(id);
+            Board board = databaseManager.Boards.SingleOrDefault(x => x.Board_ID == id);//
+            return View(board);//jeg retunere et view med variablen "board" som argument til showsiden
+        }
+    }
+}
---
+++ redesign4chan/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace redesign4chan.Controllers
+{
+    public class HomeController : Controller
+    {
+        ny4chanEntities databaseManager = new ny4chanEntities();//jeg erklærer en entiti af min database til objektet "databaseManager"
+
+        public ActionResult Index()
+        {
+            List<Board> board = databaseManager.Boards.ToList(); //her erklære jeg en liste som indeholder mine boards fra databasen og ligger dem i variablen "board"
+
+            return View(board);//jeg retunere "board" variablen sammen med view til indexsiden
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
